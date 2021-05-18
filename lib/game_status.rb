bbd327d05de3db671f4bc9bb4d4db7b8eb46b233
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-18 22:34:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -67,12 +67,3 @@ def winner(board)
     return board[winner_board[0]]
   end
 end
-
-def won?(board)
-  return false if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-  WIN_COMBINATIONS.each do |win_combo|
-    if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X" ) || (board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O" )
-      return win_combo
-    end
-  end
